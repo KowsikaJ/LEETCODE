98c9c8296fafda2c7be5cadf8127228e5dd90314
KowsikaJ <kowsikaj05@gmail.com>
2024-09-24 11:46:24 +0530

    Time: 0 ms (100%), Space: 42 MB (52.44%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        int r=matrix.length;
+        /*int r=matrix.length;
         int c=matrix[0].length;
         for(int i=0;i<r;i++)   {
             for(int j=0;j<c;j++){
@@ -9,6 +9,28 @@ class Solution {
                 }
             }
         } 
+        return false;
+        */
+        int r=matrix.length;
+        int c=matrix[0].length;
+        int le=0;
+        int ri=r*c-1;
+        while(le<=ri){
+            int mid=(le+ri)/2;
+            int row=mid/c;
+            int col=mid%c;
+            int guess=matrix[row][col];
+
+            if(target==guess){
+                return true;
+            }
+            else if(target>guess){
+                le=mid+1;
+            }
+            else{
+                ri=mid-1;
+            }
+        }
         return false;
     }
 }
