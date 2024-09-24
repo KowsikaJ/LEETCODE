30b6fae7cd16b8fe33db672178b0dcbc74623c95
KowsikaJ <kowsikaj05@gmail.com>
2024-09-24 12:19:35 +0530

    Time: 1 ms (99.39%), Space: 57.1 MB (30.19%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,6 +1,7 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        int max=Integer.MIN_VALUE,s=0;
+        //KADANE ALGORITHM
+        /*int max=Integer.MIN_VALUE,s=0;
         for(int i=0;i<nums.length;i++){
             s+=nums[i];
             if(s>max){
@@ -11,6 +12,24 @@ class Solution {
             }
         }
         return max;
+        */
+        //SLIDING WINDOW
+        int globalsum=Integer.MIN_VALUE;
+        int s=0;
+        int sum=0;
+        for(int i=0;i<nums.length;i++){
+            sum+=nums[i];
+
+            if(sum>globalsum){
+                globalsum=sum;
+            }
+            while(sum<0){
+                sum-=nums[s];
+                s++;
+            }
+
+        }
+        return globalsum;
         
     }
 }
