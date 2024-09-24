ab65967bc5756c5020db434e97d93b400540780b
KowsikaJ <kowsikaj05@gmail.com>
2024-09-24 15:40:34 +0530

    Time: 1 ms (99.2%), Space: 41.8 MB (88.65%) - LeetHub

---
+++ 1873-longest-nice-substring/1873-longest-nice-substring.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String longestNiceSubstring(String s) {
+        if (s.length() < 2) {
+            return "";  // A string of length < 2 can't be "nice"
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (s.indexOf(Character.toLowerCase(ch)) == -1 || s.indexOf(Character.toUpperCase(ch)) == -1) {
+                // Split around the current character and check both parts
+                String left = longestNiceSubstring(s.substring(0, i));
+                String right = longestNiceSubstring(s.substring(i + 1));
+                return left.length() >= right.length() ? left : right;
+            }
+        }
+        return s;  // If all characters have both upper and lower cases
+    }
+}
