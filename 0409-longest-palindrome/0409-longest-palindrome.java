519a55f56dc2ab2af6baf981111b8a78664da251
KowsikaJ <kowsikaj05@gmail.com>
2024-09-26 16:18:00 +0530

    Time: 2 ms (84.19%), Space: 41.7 MB (67.61%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.java
+++ 0409-longest-palindrome/0409-longest-palindrome.java
@@ -29,7 +29,7 @@ class Solution {
         int c=0,o=0;
         boolean flag=false;
         for(int i=0;i<arr.length;i++){
-            if(arr[i]%2==0 && arr[i]!=0){
+            if(arr[i]%2==0){
                 c+=arr[i];
             }
             else if(arr[i]%2!=0 && arr[i]!=1){
