f5f7eda2fdcd348532c9cc5fb0db3fe019168c9a
KowsikaJ <kowsikaj05@gmail.com>
2024-10-04 21:04:03 +0530

    Time: 0 ms (100%), Space: 41.7 MB (21.06%) - LeetHub

--- 0551-student-attendance-record-i/0551-student-attendance-record-i.java
+++ 0551-student-attendance-record-i/0551-student-attendance-record-i.java
@@ -1,11 +1,38 @@
 class Solution {
     public boolean checkRecord(String s) {
-        if(s.contains("LLL")){
+        /*if(s.contains("LLL")){
             return false;
         }
         if(s.indexOf('A')!=s.lastIndexOf('A')){
             return false;
         }
         return true;
+        */
+        int n=s.length()-1;
+        int p=0,l=0,a=0;
+        
+        int aCount = 0; 
+        int lCount = 0; 
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+
+            if (ch == 'A') {
+                aCount++;
+                if (aCount > 1) {
+                    return false;
+                }
+            }
+            if (ch == 'L') {
+                lCount++;
+                if (lCount > 2) {
+                    return false;
+                }
+            } else {
+                lCount = 0;
+            }
+        }
+
+        return true; 
     }
 }
