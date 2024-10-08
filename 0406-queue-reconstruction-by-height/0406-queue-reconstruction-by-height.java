2b875fd56f73aaf2de97bdea97ad56a0a31856b6
KowsikaJ <kowsikaj05@gmail.com>
2024-10-08 20:08:40 +0530

    Time: 8 ms (57.06%), Space: 45.3 MB (64.62%) - LeetHub

--- 0406-queue-reconstruction-by-height/0406-queue-reconstruction-by-height.java
+++ 0406-queue-reconstruction-by-height/0406-queue-reconstruction-by-height.java
@@ -1,14 +1,14 @@
 public class Solution {
     public int[][] reconstructQueue(int[][] people) {
         Arrays.sort(people,new Comparator<int[]>(){
-           public int compare(int[] o1, int[] o2){
-               return o1[0]!=o2[0]?-o1[0]+o2[0]:o1[1]-o2[1];
-           }
+            public int compare(int[] o1,int[] o2){
+                return o1[0]!=o2[0]?-o1[0]+o2[0]:o1[1]-o2[1];
+            }
         });
-        List<int[]> res = new LinkedList<>();
-        for(int[] cur : people){
-            res.add(cur[1],cur);       
+        List<int[]> res=new LinkedList<>();
+        for(int[] ch:people){
+            res.add(ch[1],ch);
         }
-        return res.toArray(new int[people.length][]);
+    return res.toArray(new int[people.length][]);
     }
 }
