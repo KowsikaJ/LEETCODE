6f9b847784f6a01f3e834482d9b8e52c87c30b92
KowsikaJ <kowsikaj05@gmail.com>
2024-11-21 11:50:47 +0530

    Time: 124 ms (5.92%), Space: 41.4 MB (41.68%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.java
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -1,32 +1,3 @@
-/*class Solution {
-    public boolean isSubsequence(String s, String t) {
-        int i=0;
-        int c=0;
-        int j=0;
-        if(s.length()==0){
-            return true;
-        }
-
-        while(i<t.length()){
-            if(t.charAt(i)!=s.charAt(j)){
-                i++;
-            }
-            else{
-                c++;
-                i++;
-                j++;
-                if(c==s.length())return true;
-
-            }
-        }
-        
-        return false;
-        
-    }
-}
-*/
-
-
 class Solution {
     public boolean isSubsequence(String s, String t) {
         int j=0;
@@ -41,5 +12,6 @@ class Solution {
             if(j==s.length())return true;  
         }
         return false;
+        
     }
 }
