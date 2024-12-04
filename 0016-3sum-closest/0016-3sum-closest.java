acc30d56ea1ddea015daadd35dd03ccd7e7ccd26
KowsikaJ <kowsikaj05@gmail.com>
2024-12-04 21:25:53 +0530

    Time: 14 ms (78.12%), Space: 43.3 MB (27.95%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.java
+++ 0016-3sum-closest/0016-3sum-closest.java
@@ -1,5 +1,5 @@
 //brute force approach
-class Solution {
+/*class Solution {
     public int threeSumClosest(int[] nums, int target) {
         int sum=0,s=0,d=0,diff=Integer.MAX_VALUE;
         for (int i = 0; i < nums.length - 2; i++) { // First integer
@@ -19,22 +19,37 @@ class Solution {
         
     }
 }
+*/
 
-/*class Solution {
+import java.util.Arrays;
+
+class Solution {
     public int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
         int closest = nums[0] + nums[1] + nums[2];
 
         for (int i = 0; i < nums.length - 2; i++) {
-            for (int j = i + 1; j < nums.length - 1; j++) {
-                for (int k = j + 1; k < nums.length; k++) {
-                    int currentSum = nums[i] + nums[j] + nums[k];
-                    if (Math.abs(target - currentSum) < Math.abs(target - closest)) {
-                        closest = currentSum;
-                    }
+            int left = i + 1, right = nums.length - 1;
+
+            while (left < right) {
+                int currSum = nums[i] + nums[left] + nums[right];
+
+                if (currSum == target) {
+                    return target;
+                }
+
+                if (Math.abs(currSum - target) < Math.abs(closest - target)) {
+                    closest = currSum;
+                }
+
+                if (currSum < target) {
+                    left++;
+                } else {
+                    right--;
                 }
             }
         }
 
         return closest;
     }
-}*/
+}
