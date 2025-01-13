386e699e45784f6e2ea65fe0f1c4eb41924adead
KowsikaJ <kowsikaj05@gmail.com>
2025-01-13 08:33:54 +0530

    Time: 11 ms (54.97%), Space: 46.3 MB (53.64%) - LeetHub

---
+++ 3455-minimum-length-of-string-after-operations/3455-minimum-length-of-string-after-operations.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int minimumLength(String s) {
+        if(s.length()<3) return s.length();
+
+        char[] freq=new char[26];
+        int total=0;
+        for(char ch:s.toCharArray()){
+            freq[ch-'a']++;
+        }
+
+        for(char ch:freq){
+            if(ch==0) continue;
+            if(ch%2==0) total+=2;
+            else total+=1;
+        }
+        return total;
+        
+    }
+}
