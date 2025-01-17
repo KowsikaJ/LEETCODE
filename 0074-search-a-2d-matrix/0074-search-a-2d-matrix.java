15bbb3026618ee094cd29f0890def08b20e1a1b1
Kowsika  J <kowsikaj.22csd@kongu.edu>
2025-01-17 17:59:30 +0530

    Time: 0 ms (100%), Space: 42.4 MB (36.91%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,37 +1,28 @@
 class Solution {
-    public boolean searchMatrix(int[][] matrix, int target) {
-        /*int r=matrix.length;
-        int c=matrix[0].length;
-        for(int i=0;i<r;i++)   {
-            for(int j=0;j<c;j++){
-                if(target==matrix[i][j]){
-                    return true;
-                }
-            }
-        } 
-        return false;
-        */
-        int r=matrix.length;
-        int c=matrix[0].length;
-        int le=0;
-        int ri=r*c-1;
-        while(le<=ri){
-            int mid=(le+ri)/2;
-            int row=mid/c;
-            int col=mid%c;
-            int guess=matrix[row][col];
-
-            if(target==guess){
+    public static boolean binarySearch(int[] array,int target){
+        int left = 0;
+        int right = array.length-1;
+        int mid = left+(right-left)/2;
+        while(left<=right){
+            mid=left+(right-left)/2;
+            if(target==array[mid])
                 return true;
-            }
-            else if(target>guess){
-                le=mid+1;
-            }
-            else{
-                ri=mid-1;
-            }
+            else if(target<array[mid])
+                right=mid-1;
+            else
+                left=mid+1;
         }
         return false;
     }
+    
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int n=matrix[0].length;
+        boolean flag=false;
+        for(int i=0;i<matrix.length;i++){
+            if(target>=matrix[i][0] && target<=matrix[i][n-1]){
+                return binarySearch(matrix[i],target);
+            }
+        }
+        return flag;
+    }
 }
-        
