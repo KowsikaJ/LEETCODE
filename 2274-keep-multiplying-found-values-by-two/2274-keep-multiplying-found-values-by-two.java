6175af36d1587a6ebe7a6007d614a7bd94f1b643
Kowsika  J <kowsikaj.22csd@kongu.edu>
2025-01-27 23:40:15 +0530

    Time: 1 ms (91.36%), Space: 44.2 MB (59.49%) - LeetHub

--- 2274-keep-multiplying-found-values-by-two/2274-keep-multiplying-found-values-by-two.java
+++ 2274-keep-multiplying-found-values-by-two/2274-keep-multiplying-found-values-by-two.java
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
     public int findFinalValue(int[] nums, int original) {
         HashSet<Integer> set = new HashSet<>();
         for (int num : nums) {
@@ -8,6 +8,28 @@ class Solution {
             original *= 2;
         }
 
+        return original;
+    }
+}
+*/
+class Solution {
+    public int findFinalValue(int[] nums, int original) {
+        int n = nums.length;
+        
+        while (true) {
+            boolean found = false;
+            for (int i = 0; i < n; i++) {
+                if (nums[i] == original) {
+                    original *= 2;
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) {
+                break;
+            }
+        }
+        
         return original;
     }
 }
